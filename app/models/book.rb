92fbd710a2160c236519c3268acd9d22ff495030
Daniel Grijuela <danielgrijuela@gmail.com>
2014-10-09 01:50:02 +0200

    update book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,23 @@
 class Book < ActiveRecord::Base
+  belongs_to :user
+
+  # Paperclip gem methods for the cover image
+  has_attached_file :cover,
+                    styles: {
+                      grid: '200x150' # Image size to use in grids (e.g. home page)
+                    }
+  validates_attachment :cover,
+                       presence: true,
+                       content_type: { content_type: /^image\/(jpeg|png|gif|tiff)$/ }
+
+  # BookAuthorRelation table specifies relations between the Book table & the Author table
+  has_many :book_author_relations
+  accepts_nested_attributes_for :book_author_relations
+
+  has_many :authors, through: :book_author_relations
+
+  has_many :attachments, dependent: :destroy
+  accepts_nested_attributes_for :attachments
+
+  validates_presence_of :title
 end
